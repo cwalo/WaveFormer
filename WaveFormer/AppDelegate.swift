93fcef3548e74d98567aa3f6bc0e2c86aef7b3c6
Corey Walo <cjwalo@gmail.com>
2016-11-06 17:50:29 -0600

    Initial Commit

---
+++ WaveFormer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WaveFormer
+//
+//  Created by Corey Walo on 11/6/16.
+//  Copyright © 2016 Audio Armada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
